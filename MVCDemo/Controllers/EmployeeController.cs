5594da52b26d41d7e34ac2e81f2cbb13006fd5c5
mythr <mythr@LAPTOP-78LDJ37C>
2024-10-20 19:39:41 -0700

    Add project files.

---
+++ MVCDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            List<Employee> obj = new List<Employee>()
+            {
+                new Employee(){ Empid=101,Empname="Sharmila",Empsal=10000,Empaddress="Chennai"},
+                new Employee(){ Empid=102,Empname="Sam",Empsal=50000,Empaddress="Texas"},
+                new Employee(){ Empid=103,Empname="Daisy",Empsal=30000,Empaddress="bombay"},
+                new Employee(){ Empid=104,Empname="Wyatt",Empsal=20000,Empaddress="Bangalore"},
+                new Employee(){ Empid=105,Empname="Kim",Empsal=70000,Empaddress="California"},
+            };
+            return View(obj);
+        }
+    }
+}
---
+++ MVCDemo/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Models
+{
+    public class Employee
+    {
+        public int Empid { get; set; }
+        public string Empname { get; set; }
+        public int Empsal { get; set; }
+        public string Empaddress { get; set; }
+    }
+}
